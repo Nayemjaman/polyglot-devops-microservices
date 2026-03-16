20d99f12ef49aa5f1f26d4af7218f2ad29bcc590
Nayemjaman <nayemjamantusher@gmail.com>
2026-03-16 14:14:40 +0600

    initilize auth services with Django rest framework backend | project stracture | and cross setup

---
+++ services/auth-service-drf/users/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# super admin will create stuff users and the customer user register with their mail
+# JWT, login logout, communicate other microservices
